aebddb67cf1f7e0f1abbb37878b1151002619a9b
interpol <mrpalich@gmail.com>
2022-12-06 20:39:14 +0300

    Kinda meh 3.2 solution

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 fn main() {
-    println!("{:?}", solve().unwrap_or(0));
+    println!("{:?}", solve_second().unwrap_or(0));
 }
 
 fn solve() -> Result<u32, std::io::Error> {
@@ -37,6 +37,43 @@ fn solve() -> Result<u32, std::io::Error> {
         //println!("{:?} {:?}", sum, diff);
 
         result += sum;
+    }
+    Ok(result)
+}
+
+fn solve_second() -> Result<u32, std::io::Error> {
+    let lines = BufReader::new(File::open("src/input")?).lines();
+
+    let mut result: u32 = 0;
+
+    let mut rucksacks: Vec<Vec<char>> = Vec::new();
+
+    for line in lines {
+        rucksacks.push(line?.to_string().chars().collect::<Vec<_>>());
+
+        if rucksacks.len() > 2 {
+            let mut diff: Vec<&char> = rucksacks[0]
+                .iter()
+                .filter(|k| rucksacks[1].contains(k) && rucksacks[2].contains(k))
+                .collect();
+
+            diff.dedup();
+            let mut sum: u32 = 0;
+
+            for item in &diff {
+                if item.is_uppercase() {
+                    sum += (**item as u32 & 0x1F) + 26;
+                } else {
+                    sum += **item as u32 & 0x1F;
+                }
+            }
+
+            println!("{:?} {:?}", sum, diff);
+
+            result += sum;
+
+            rucksacks.clear()
+        }
     }
     Ok(result)
 }
