c904235663c50f9db5137a335a9926b0ccc22abe
interpol <mrpalich@gmail.com>
2022-12-09 21:28:43 +0300

    Speed some things up for day6

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,7 +1,6 @@
 use std::{
     collections::HashSet,
     fs::File,
-    hash::Hash,
     io::{BufRead, BufReader},
 };
 
@@ -9,30 +8,24 @@ fn main() {
     println!("{:?}", solve().unwrap_or_default());
 }
 
-fn solve() -> Result<i32, std::io::Error> {
-    let mut line = BufReader::new(File::open("src/input")?).lines();
-    let line = line.next().unwrap()?;
-
-    let mut sequence = Vec::new();
+fn solve() -> Result<usize, std::io::Error> {
+    let line = BufReader::new(File::open("src/input")?);
+    let line = line.lines().next().unwrap()?.into_bytes();
     let needle_size = 14;
 
-    for c in line.chars().enumerate() {
-        sequence.push(c.1);
+    let answer = line
+        .windows(needle_size)
+        .enumerate()
+        .take_while(|(_, str)| !unique(str))
+        .last()
+        .unwrap()
+        .0;
 
-        let is_unique = unique(&mut sequence);
-
-        if sequence.len() == needle_size && is_unique {
-            println!("Found {:?}", sequence);
-            return Ok(c.0 as i32 + 1);
-        } else if sequence.len() == needle_size {
-            _ = sequence.remove(0);
-        }
-    }
-    Ok(0)
+    Ok(answer + needle_size + 1)
 }
 
-fn unique<T: Eq + Hash + Copy>(v: &mut [T]) -> bool {
-    let mut uniques: HashSet<T> = HashSet::new();
+fn unique(v: &[u8]) -> bool {
+    let mut uniques = HashSet::new();
     for f in v.iter() {
         if !uniques.insert(*f) {
             return false;
